bcca6780d6de6204e18261283beed14b4b5ce627
Mia <miazbikowski@gmail.com>
2014-03-11 17:33:16 -0400

    Finished up static site

--- molten/molten/urls.py
+++ molten/molten/urls.py
@@ -5,7 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'molten.views.home', name='home'),
+    url(r'^$', 'core.views.index', name='index'),
+    url(r'^about/$', 'core.views.about', name='about'),
+    url(r'^projects/$', 'core.views.projects', name='projects'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
